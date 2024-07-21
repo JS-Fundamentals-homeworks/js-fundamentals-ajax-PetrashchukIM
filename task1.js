a57ae067439466a3f188a4d5ae8d9ba2bf0d9972
PetrashchukIM <petrashchukim@gmail.com>
2024-07-21 14:45:35 +0300

    finished task2

--- task1.js
+++ task1.js
@@ -1,31 +1,27 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
 
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => {
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => {
     if (!res.ok) {
-        throw new Error ('response wasn`t ok: ' + res.statusText );
-    }    
+      throw new Error("response wasn`t ok: " + res.statusText);
+    }
     return res.json();
-})
-  .then(data => {
-    const names = data.map(user => user.name);
-    const elUl = document.getElementsByClassName('usersList')[0];
-    names.forEach(name => {
-        const elLi = document.createElement('li');
-        elLi.textContent = name;
-        elUl.appendChild(elLi);
+  })
+  .then((data) => {
+    const names = data.map((user) => user.name);
+    const elUl = document.getElementsByClassName("usersList")[0];
+    names.forEach((name) => {
+      const elLi = document.createElement("li");
+      elLi.textContent = name;
+      elUl.appendChild(elLi);
     });
-    
-})
-  .catch(error =>{
-    console.error('You have error:', error)
-});
-  
-
+  })
+  .catch((error) => {
+    console.error("You have error:", error);
+  });
--- task2.js
+++ task2.js
@@ -5,3 +5,32 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.getElementById("getUserButton").onclick = getCity;
+
+function getCity() {
+
+  const userName = document.getElementById("userNameInput").value;
+  if (userName === "") {
+    document.getElementById("userCity").textContent = "Please enter user name.";
+    return; 
+  }  
+
+  console.log(userName);  
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Response wasn`t ok: " + res.statusText);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const userNameInfo = data.filter(user => user.name == userName)
+      const userCity = userNameInfo.map(user => user.address.city);
+      document.getElementById("userCity").textContent = userCity[0];
+    })
+    .catch((error) => {
+      console.error("You have error:", error);
+    });
+}
+
