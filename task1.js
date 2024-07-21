4a06566dbca07dec89c477ae8d42182b9b968055
PetrashchukIM <petrashchukim@gmail.com>
2024-07-21 13:59:47 +0300

    finished task1

--- task1.js
+++ task1.js
@@ -5,3 +5,27 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    if (!res.ok) {
+        throw new Error ('response wasn`t ok: ' + res.statusText );
+    }    
+    return res.json();
+})
+  .then(data => {
+    const names = data.map(user => user.name);
+    const elUl = document.getElementsByClassName('usersList')[0];
+    names.forEach(name => {
+        const elLi = document.createElement('li');
+        elLi.textContent = name;
+        elUl.appendChild(elLi);
+    });
+    
+})
+  .catch(error =>{
+    console.error('You have error:', error)
+});
+  
+
